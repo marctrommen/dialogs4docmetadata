455842324ccef8cad8c191ca8c02c4a53d6f645b
Marcus Trommen <marcus.trommen@gmx.net>
2021-05-26 07:11:34 +0200

    suchergebnisse nun mit less und Vorbelegung über clipit

--- search_archive.sh
+++ search_archive.sh
@@ -9,6 +9,8 @@ source ./config_handler.sh
 BACKTITEL="Dokumentarchivierung"
 TITEL="Textmuster in Metadaten suchen"
 SEARCH_PATTERN=""
+CLIPBOARD=/usr/bin/clipit
+
 
 # -------------------------------
 # FUNCTIION get_search_pattern ()
@@ -21,19 +23,17 @@ SEARCH_PATTERN=""
 #     1 ... user pushed <ESC> key
 # -------------------------------
 function get_search_pattern {
-
-	ANSWER=""
-	while [ "${ANSWER}" = "" ] ; do
-		# Generate the dialog box
-		ANSWER=$( \
-			$DIALOG \
-			--backtitle "${BACKTITEL}" \
-			--title "${TITEL}" \
-			--no-cancel \
-			--inputbox "${1}" 5 45 "${SEARCH_PATTERN}" \
-			3>&1 1>&2 2>&3 \
-		)
-	done
+	local ANSWER=""
+	
+	# Generate the dialog box
+	ANSWER=$( \
+		$DIALOG \
+		--backtitle "${BACKTITEL}" \
+		--title "${TITEL}" \
+		--no-cancel \
+		--inputbox "${1}" 5 45 "${SEARCH_PATTERN}" \
+		3>&1 1>&2 2>&3 \
+	)
 
 	# Get the exit status
 	DIALOG_EXIT_STATUS=$?
@@ -51,49 +51,80 @@ function get_search_pattern {
 	esac
 }
 
+
 # -------------------------------
 # FUNCTIION search_results ()
 # run the search with the given pattern SEARCH_PATTERN 
-# and show the results in a prgbox
+# and pipe the results into less
 # INPUT 
-#     $1 ... label: textlabel of dialog
+#     none
 # RETURN:
 #     0 ... okay
 #     1 ... user pushed <ESC> key
 # -------------------------------
 function search_results {
+	SCAN_DOCUMENT_TITLE=""
+	
 	find "${SCAN_ARCHIVE_BASE_DIRECTORY}" -type f -name '*.json' \
 		| xargs grep -h "title" \
 		| grep -h -i "${SEARCH_PATTERN}" \
 		| awk -F: '{ print $2 }' \
-		| uniq | sort \
-		> ./found.txt
+		| sort --unique \
+		| less
 	
-	# Generate the dialog box
-	ANSWER=$( \
-		$DIALOG \
-		--backtitle "${BACKTITEL}" \
-		--title "${TITEL}" \
-		--no-cancel \
-		--scrollbar \
-		--editbox ./found.txt 18 65 \
-		3>&1 1>&2 2>&3 \
-	)
-
 	# Get the exit status
 	DIALOG_EXIT_STATUS=$?
 	
-	rm -f ./found.txt
-
 	# Handle exit status
-	case ${DIALOG_EXIT_STATUS} in
-	$BUTTON_OK)
-		return 0
-		;;
-	$KEY_ESC)
-		return 1
-		;;
-	esac
+	if [ ${DIALOG_EXIT_STATUS} -eq $BUTTON_OK ]; then
+		which ${CLIPBOARD}
+		return=$?
+		if [ $return -eq 0 ]; then
+			SCAN_DOCUMENT_TITLE=$(${CLIPBOARD} -c)
+			return 0
+		fi
+	fi
+
+	return 1
+}
+
+
+# -------------------------------
+# FUNCTIION set_document_title ()
+# let the user enter text for the metadata document title.
+# affects the variable SCAN_DOCUMENT_TITLE
+# INPUT
+#     $1 ... label: textlabel of dialog
+# RETURN:
+#     0 ... okay
+#     1 ... user pushed <ESC> key
+# -------------------------------
+function set_document_title {
+	local ANSWER=""
+	
+	while [ "${ANSWER}" = "" ]; do
+		# Generate the dialog box
+		ANSWER=$( \
+			$DIALOG \
+			--backtitle "${BACKTITEL}" \
+			--title "${TITEL}" \
+			--no-cancel \
+			--inputbox "${1}" 0 0 "${SCAN_DOCUMENT_TITLE}" \
+			3>&1 1>&2 2>&3 \
+		)
+	
+		# Get the exit status
+		DIALOG_EXIT_STATUS=$?
+
+		# Handle exit status
+		if [ ${DIALOG_EXIT_STATUS} -eq $BUTTON_OK ]; then
+			# Handle dialog output
+			SCAN_DOCUMENT_TITLE="$ANSWER"
+			return 0
+		fi
+	done
+	
+	return 1
 }
 
 # -------------------------------
@@ -102,25 +133,34 @@ function search_results {
 ready=1
 return=0
 
-while [ $ready = 1 ] ; do
+while [ $ready = 1 ]; do
+	# search in the meta data of the docarchive?
 	get_search_pattern "Suchmuster?"
 	return=$?
 	
-	if [ $return -eq $BUTTON_OK ] ; then
-		search_results "Suchergebnisse"
-		return=$?
+	if [ $return -eq $BUTTON_OK ]; then
+		# do a text search only if SEARCH_PATTERN is not empty
+		if [ "${SEARCH_PATTERN}" != "" ] ; then
+			search_results
+			return=$?
+		fi
 	fi
 
-	if [ $return -eq $BUTTON_OK ] ; then
+	if [ $return -eq $BUTTON_OK ]; then
+		set_document_title "Dokumenttitel / Betreff? *\n\nDarf nicht leer sein!\n"
+		return=$?
+	fi
+	
+	if [ $return -eq $BUTTON_OK ]; then
 		dialog \
 		--backtitle "${BACKTITEL}" \
 		--title "${TITEL}" \
-		--yesno "Suchergebnisse okay?" 0 0
+		--yesno "Dokumenttitel okay?" 0 0
 		
 		return=$?
 		
 		# <ESC> key pressed?
-		if [ $return -eq $KEY_ESC ] ; then
+		if [ $return -eq $KEY_ESC ]; then
 			ready=0
 		else
 			ready=$return
@@ -130,12 +170,15 @@ while [ $ready = 1 ] ; do
 	fi
 done
 
-if [ $return -eq $BUTTON_OK ] ; then
-	
+if [ $return -eq $BUTTON_OK ]; then
+	echo "Gewählter Dokumenttitel:"
+	echo "${SCAN_DOCUMENT_TITLE}"
 	write_config
 	exit 0
 fi
 
 #clear
 echo "Abbruch durch Benutzer!"
+SCAN_DOCUMENT_TITLE=""
+write_config
 exit 1
